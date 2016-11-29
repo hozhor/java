3ac778c26923d56df4a1c947e9aed86b850d9faf
hozhor <hozhor@gmail.com>
2016-11-29 23:09:49 +0800

    写一个函数，求一个字符串的长度，在main函数中输入字符串，并输出其长度。   

---
+++ lianxi38.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class lianxi38 {
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("请输入一个字符串：");
+    String str = s.nextLine();
+     System.out.println("字符串的长度是："+str.length());
+    }
+    }
