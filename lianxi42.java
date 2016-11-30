589335c68b9b02cbbfcd5489ae67886af61cb0e3
hozhor <hozhor@gmail.com>
2016-12-01 07:41:38 +0800

    809*??=800*??+9*??+1    其中??代表的两位数,8*??的结果为两位数，9*??的结果为3位数。求??代表的两位数，及809*??后的结果。   

---
+++ lianxi42.java
@@ -0,0 +1,9 @@
+public class lianxi42 { 
+public static void main (String[] args) { 
+int a=809,b,i;
+for(i=10;i<13;i++)
+{b=i*a ;
+if(8*i<100&&9*i>=100)
+System.out.println ("809*"+i+"="+"800*"+i+"+"+"9*"+i+"="+b);}
+}
+} 
