9839aea879ff9312ad1b064c219813c400b49204
hozhor <hozhor@gmail.com>
2017-03-17 22:40:39 +0800

    有关方法的基本练习题

---
+++ FunDemo02.java
@@ -0,0 +1,11 @@
+public class FunDemo02{
+public static void main(String[] args){
+System.out.println("max:"+compare(7,10));
+}
+//两个数比较的方法
+public static int compare(int a,int b){
+return (a>b? a:b);
+
+}
+
+}
